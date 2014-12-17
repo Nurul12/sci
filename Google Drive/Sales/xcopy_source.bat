aa562911cabd6c5bb40aeaf40d617ae14a9bd62d
wahyudistir4 <wahyudistira.wd16@gmail.com>
2014-12-17 11:26:21 +0700

    X

---
+++ Google Drive/Sales/xcopy_source.bat
@@ -0,0 +1,9 @@
+copy *.ico copy_source
+copy *.png copy_source
+copy *.pbl copy_source
+copy *.pbt copy_source
+copy *.pbw copy_source
+copy framework\fw_objects.pbl copy_source
+copy framework\fw_objects.pbd copy_source
+
+
